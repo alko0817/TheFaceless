b2690873036271b76107b4c59ccae7cdf4a816c0
Fred Azzopardi <fred.azzo1997@live.co.uk>
2020-04-01 12:34:51 +0200

    Working on basic ai
    renamed Scenes to _Scenes
    written basic ai controller. has a "sight" range and goes towards player when in sight range.

---
+++ TheFacelessTest/Assets/Scripts/AIController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+
+    GameObject player;
+    public float chaseDistance;
+    public float attackDistance;
+    public float speed;
+
+    private float distanceToPlayer;
+
+    private void Update()
+    {
+        player = GameObject.FindWithTag("Player");
+
+        Vector3 vectorToPlayer = player.transform.position - transform.position;
+
+        distanceToPlayer = vectorToPlayer.magnitude;
+
+        if (distanceToPlayer < chaseDistance)
+        {
+            float startTime = Time.time; 
+
+            Debug.Log(this.name + " sees " + player.name);
+            //float distCovered = (Time.time - startTime) * speed;
+            //float fractionOfJourney = distCovered / distanceToPlayer;
+
+            //transform.position = Vector3.Lerp(this.transform.position, player.transform.position, fractionOfJourney);
+
+
+            Vector3 direction = player.transform.position - transform.position;
+            direction.Normalize();
+
+            if (distanceToPlayer > attackDistance)
+            {
+                transform.position = transform.position + (direction * speed * Time.deltaTime);
+            }
+            
+        }
+
+
+
+    }
+
+
+
+}
