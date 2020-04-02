68abe625b118e35735cb018b58f9be8d4051b717
Fred Azzopardi <fred.azzo1997@live.co.uk>
2020-04-02 15:03:04 +0200

    started on ai behaviour

---
+++ TheFacelessTest/Assets/Scripts/AIBehaviour.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIBehaviour : MonoBehaviour
+{
+    public float sightDistance;
+    public float attackDistance;
+    public float speed;
+
+    private float senseTimer;
+    private float actTimer;
+
+    GameObject player;
+
+    private bool playerDetected;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerDetected = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void Sense(float dt)
+    {
+        Vector3 vectorToPlayer = player.transform.position - transform.position;
+        float distanceToPlayer = vectorToPlayer.magnitude;
+        
+        if(distanceToPlayer < sightDistance)
+        {
+            playerDetected = true;
+        }
+        else
+        {
+            playerDetected = false;
+        }
+    }
+
+    void Decide()
+    {
+
+    }
+
+    void Act(float dt)
+    {
+
+    }
+
+    void Attack()
+    {
+
+    }
+
+    void MoveTo(Transform location)
+    {
+
+    }
+}
