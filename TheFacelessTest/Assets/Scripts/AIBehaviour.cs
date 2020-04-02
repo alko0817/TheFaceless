16efec3a5d74e21e6f184903982ff1e8aa99e43a
Fred Azzopardi <fred.azzo1997@live.co.uk>
2020-04-02 15:48:42 +0200

    working on sense decide act loop of ai

--- TheFacelessTest/Assets/Scripts/AIBehaviour.cs
+++ TheFacelessTest/Assets/Scripts/AIBehaviour.cs
@@ -8,53 +8,86 @@ public class AIBehaviour : MonoBehaviour
     public float attackDistance;
     public float speed;
 
-    private float senseTimer;
-    private float actTimer;
 
-    GameObject player;
+    private GameObject player;
+    private Transform lastKnownPlayerLocation;
+    private float distanceToPlayer;
+
+    public float senseFrequency;
+    private float senseTimer;
 
     private bool playerDetected;
-
+    private bool moveToPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
         playerDetected = false;
+        moveToPlayer = false;
+        senseTimer = 0.0f;
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 vectorToPlayer = player.transform.position - transform.position;
+        distanceToPlayer = vectorToPlayer.magnitude;
+
+        senseTimer += Time.deltaTime;
+
+        if(senseTimer > senseFrequency)
+        {
+            senseTimer = 0.0f;
+            Sense();
+        }
+
+        Decide();
+        Act();
     }
 
 
-    void Sense(float dt)
+    void Sense()
     {
-        Vector3 vectorToPlayer = player.transform.position - transform.position;
-        float distanceToPlayer = vectorToPlayer.magnitude;
-        
-        if(distanceToPlayer < sightDistance)
+
+        if (distanceToPlayer < sightDistance)
         {
             playerDetected = true;
+            lastKnownPlayerLocation = player.transform;
+            Debug.Log("Player sighted by " + gameObject.name);
         }
         else
         {
             playerDetected = false;
+            Debug.Log(gameObject.name + " lost sight of Player");
         }
+
     }
 
     void Decide()
     {
-
+        if (playerDetected && distanceToPlayer > attackDistance)
+        {
+            moveToPlayer = true;
+        }
     }
 
-    void Act(float dt)
+    void Act()
     {
+        if(moveToPlayer)
+        {
+            MoveTo(lastKnownPlayerLocation);
+        }
+
 
     }
 
     void Attack()
+    {
+
+    }
+
+    void Patrol()
     {
 
     }
