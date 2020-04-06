1697e989e31df52546ea3cc02cce711d7d429c59
Alex  Kokkinidis <alexandros.kokkinidis.0817@student.uu.se>
2020-04-06 12:16:07 +0200

    scene Triggers implemented

--- TheFacelessTest/Assets/sceneTrigger.cs
+++ TheFacelessTest/Assets/sceneTrigger.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class sceneTrigger : MonoBehaviour
 {
     public GameObject player;
+    public GameObject panel;
+    public TextMeshProUGUI text;
+    public Animator UIAnim;
+    public float fadeOutDelay = 1f;
+
+
+    //private void Awake()
+    //{
+    //    panel.SetActive(false);
+    //    text.enabled = false;
+    //}
 
     //SOME UI ELEMENTS
     //ADD EFFECTS?
@@ -18,6 +30,20 @@ public class sceneTrigger : MonoBehaviour
         if (other.gameObject.tag == player.tag)
         {
             Debug.Log("This is your story... " + player.tag);
+
+            //panel.SetActive(true);
+            //text.enabled = true;
+            UIAnim.SetTrigger("fadeIn");
+            StartCoroutine("FadeOut");
+
         }
     }
+
+    IEnumerator FadeOut ()
+    {
+        yield return new WaitForSeconds(fadeOutDelay);
+
+        UIAnim.SetTrigger("fadeOut");
+    }
+
 }
