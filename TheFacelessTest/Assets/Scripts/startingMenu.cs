000b600a140f87b79e6f139ffb84aee5fe58e7e7
Alex  Kokkinidis <alexandros.kokkinidis.0817@student.uu.se>
2020-04-10 17:35:16 +0200

    (fuckGit) starting meny

--- TheFacelessTest/Assets/Scripts/startingMenu.cs
+++ TheFacelessTest/Assets/Scripts/startingMenu.cs
@@ -18,7 +18,7 @@ public class startingMenu : MonoBehaviour
     IEnumerator Delay ()
     {
         animator.SetTrigger("play");
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1.2f);
         SceneManager.LoadScene(1);
     }
 
