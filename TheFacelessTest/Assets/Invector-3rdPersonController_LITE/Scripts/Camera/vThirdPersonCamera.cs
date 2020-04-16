94461fdc10619ea5922256126bb5c4f83c868522
Alex  Kokkinidis <alexandros.kokkinidis.0817@student.uu.se>
2020-04-16 10:19:06 +0200

    stuff

--- TheFacelessTest/Assets/Invector-3rdPersonController_LITE/Scripts/Camera/vThirdPersonCamera.cs
+++ TheFacelessTest/Assets/Invector-3rdPersonController_LITE/Scripts/Camera/vThirdPersonCamera.cs
@@ -69,7 +69,7 @@ public class vThirdPersonCamera : MonoBehaviour
         if (target == null)
             return;
 
-        _camera = GetComponentInChildren<Camera>();
+        _camera = GetComponent<Camera>();
         currentTarget = target;
         currentTargetPos = new Vector3(currentTarget.position.x, currentTarget.position.y + offSetPlayerPivot, currentTarget.position.z);
 
---
+++ TheFacelessTest/Assets/Scripts/followCam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followCam : MonoBehaviour
+{
+    public Transform cam;
+    Transform holder;
+    public float smooth;
+
+    private void Start()
+    {
+        holder = GetComponent<Transform>();
+        
+    }
+
+    private void Update()
+    {
+        holder.position = Vector3.Lerp(holder.position, cam.position, smooth * Time.deltaTime);
+    }
+}
