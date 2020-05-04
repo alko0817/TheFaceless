6a2b784995c8f7d6af540605e7ae2333ab7903cf
Alex  Kokkinidis <alexandros.kokkinidis.0817@student.uu.se>
2020-05-04 12:08:54 +0200

    SWORD TEXTURES IN! + some editor stuff

---
+++ TheFacelessTest/Assets/Editor/PlayerControllerWindow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class PlayerControllerWindow : EditorWindow
+{
+
+    [MenuItem("Window/Player Controller")]
+    public static void ShowWindow ()
+    {
+        GetWindow<PlayerControllerWindow>("Player Controller");
+    }
+
+
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Controller", EditorStyles.boldLabel);
+        GUILayout.Box("player stuff");
+        
+    }
+}
