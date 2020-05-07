ea0d65205fc5db276ab5934d494f799c67e18a67
Alex  Kokkinidis <alexandros.kokkinidis.0817@student.uu.se>
2020-05-07 12:36:48 +0200

    getting ready

--- TheFacelessTest/Assets/FaceState.cs
+++ TheFacelessTest/Assets/FaceState.cs
@@ -5,19 +5,19 @@ using UnityEngine.UI;
 
 public class FaceState : MonoBehaviour
 {
-    public Sprite aggro, death;
-    public Image bg;
-    public Image state;
-    public Animator anim;
+    //public Sprite aggro, death;
+    //public Image bg;
+    //public Image state;
+    //public Animator anim;
 
-    AIBehaviour controller;
+    //AIBehaviour controller;
 
-    private void Start()
-    {
-        controller = GetComponent<AIBehaviour>();
-        state.sprite = aggro;
-        state.enabled = false;
+    //private void Start()
+    //{
+    //    controller = GetComponent<AIBehaviour>();
+    //    state.sprite = aggro;
+    //    state.enabled = false;
 
-    }
+    //}
 
 }
