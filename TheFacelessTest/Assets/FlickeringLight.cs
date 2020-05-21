abacf3a2705e1b91460c16d42c3dd366ce7510e4
Alex  Kokkinidis <alexandros.kokkinidis.0817@student.uu.se>
2020-05-21 21:22:39 +0200

    setting up

---
+++ TheFacelessTest/Assets/FlickeringLight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+    public Light flick;
+    public float minWaitTime;
+    public float maxWaitTime;
+    public bool useFlickering;
+    [Space]
+    public MeshRenderer mesh;
+    public Material dark;
+    public Material bright;
+
+    private float timer;
+
+    private void Update()
+    {
+        if (!useFlickering) return;
+
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            timer = Random.Range(minWaitTime, maxWaitTime);
+            flick.enabled = !flick.enabled;
+            if (flick.enabled) mesh.material = bright;
+            else mesh.material = dark;
+
+        }
+    }
+}
