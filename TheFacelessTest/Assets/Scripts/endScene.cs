8fc97b6da52af8c07bfc3e9274bb1885e9b16028
Fred Azzopardi <fred.azzo1997@live.co.uk>
2020-05-26 17:09:28 +0200

    End scene!!

---
+++ TheFacelessTest/Assets/Scripts/endScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endScene : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(scene());
+    }
+
+
+    IEnumerator scene()
+    {
+        yield return new WaitForSeconds(10f);
+        anim.SetTrigger("nextScene");
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(0);
+
+    }
+}
