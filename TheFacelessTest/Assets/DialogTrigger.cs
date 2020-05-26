4aa71c58a052de882acd7b0f7edf43a8d6f04bdb
Alex  Kokkinidis <alexandros.kokkinidis.0817@student.uu.se>
2020-05-26 16:24:02 +0200

    greta talking

---
+++ TheFacelessTest/Assets/DialogTrigger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    GameObject player;
+    AudioSource sound;
+    public AudioClip entity;
+    public AudioClip nicola;
+
+    public Animator face;
+    public GameObject greta;
+
+    bool triggered = false;
+    bool answer = false;
+
+    private void Start()
+    {
+        sound = GetComponent<AudioSource>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (player.CompareTag(other.tag))
+        {
+            if (!triggered)
+            {
+                triggered = true;
+                Face();
+                StartCoroutine(Dialog());
+            }
+        }
+    }
+
+    void Face()
+    {
+        greta.SetActive(true);
+        face.SetTrigger("talk");
+    }
+
+    IEnumerator Dialog()
+    {
+        sound.Play();
+        while (sound.isPlaying)
+        {
+            yield return null;
+        }
+        answer = true;
+        greta.SetActive(false);
+    }
+}
