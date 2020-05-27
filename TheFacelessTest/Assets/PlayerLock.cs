c50ca7d757004a54c020ffb10a63987507a219ea
Alex  Kokkinidis <alexandros.kokkinidis.0817@student.uu.se>
2020-05-27 15:06:11 +0200

    fixing shit

--- TheFacelessTest/Assets/PlayerLock.cs
+++ TheFacelessTest/Assets/PlayerLock.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class PlayerLock : MonoBehaviour
 {
+   
     playerController controller;
     public vThirdPersonCamera tpc;
     public Transform detectArea;
@@ -13,6 +16,7 @@ public class PlayerLock : MonoBehaviour
 
     bool targeting = false;
     Transform target;
+    GameObject enemy;
 
     private void Start()
     {
@@ -22,16 +26,25 @@ public class PlayerLock : MonoBehaviour
 
     private void Update()
     {
+        Detect();
+        Track();
+        Target();
+    }
+
+    void Detect ()
+    {
         Collider[] enemies = Physics.OverlapSphere(detectArea.position, detectRadius, controller.enemyLayer);
 
         if (enemies == null) target = null;
 
-        foreach(Collider enemy in enemies)
+        foreach (Collider enemy in enemies)
         {
             target = enemy.transform;
         }
+    }
 
-
+    void Track ()
+    {
         if (Input.GetKeyDown(KeyCode.Tab) && !targeting)
         {
             if (target != null)
@@ -43,6 +56,16 @@ public class PlayerLock : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.Tab) && targeting)
         {
             targeting = false;
+        }
+    }
+
+    void Target()
+    {
+        if (targeting)
+        {
+            transform.LookAt(target);
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, transform.eulerAngles.z);
+            if (!target.gameObject.activeInHierarchy) targeting = false;
         }
     }
 }
